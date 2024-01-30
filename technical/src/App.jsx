6c0c18569eedd1038cf9c2aa675aeec5566b433b
omarlaneor <annielaneor@gmail.com>
2024-01-29 21:50:39 -0500

    First Commit to Project

---
+++ technical/src/App.jsx
@@ -0,0 +1,28 @@
+import { useCatImage } from "./hooks/useCatImage.js";
+import { useCatFact } from "./hooks/useCatFact.js";
+import "../style.css";
+
+export function App() {
+  const { fact, refreshFact } = useCatFact();
+  const { imageUrl } = useCatImage({ fact });
+
+  const handleClick = async () => {
+    refreshFact();
+  };
+
+  return (
+    <main>
+      <h1>Mininos</h1>
+
+      <button onClick={handleClick}>Get new fact</button>
+
+      {fact && <p>{fact}</p>}
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt={`Image extracted using the first three words for ${fact}`}
+        />
+      )}
+    </main>
+  );
+}
---
+++ technical/src/components/Otro/Otro.jsx
@@ -0,0 +1,8 @@
+import { useCatImage } from "../../hooks/useCatImage";
+
+export function Otro() {
+  const { imageUrl } = useCatImage({ fact: "cat" });
+  console.log(imageUrl);
+
+  return <>{imageUrl && <img src={imageUrl} />}</>;
+}
